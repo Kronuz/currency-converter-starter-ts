be44d55652664cf768a3d6992c0d69ed7cf25973
German M. Bravo <german.mb@gmail.com>
2018-04-23 14:30:46 -0500

    Home Screen: Header II

--- app/components/Header/Header.js
+++ app/components/Header/Header.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { View, TouchableOpacity, Image } from 'react-native';
 
+import styles from './styles';
 import imageGear from './images/gear.png';
 
 const Header = () => (
-  <View>
-    <TouchableOpacity>
-      <Image resizeMode="contain" source={imageGear} />
+  <View style={styles.container}>
+    <TouchableOpacity style={styles.button}>
+      <Image style={styles.icon} resizeMode="contain" source={imageGear} />
     </TouchableOpacity>
   </View>
 );
