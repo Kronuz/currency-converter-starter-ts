d4f295524fa4a572039fc7fbd6226300e1d3f85d
German M. Bravo <german.mb@gmail.com>
2018-04-26 11:27:45 -0500

    Options Screen II

--- app/screens/Options.js
+++ app/screens/Options.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
-import { ScrollView, StatusBar } from 'react-native';
+import { ScrollView, StatusBar, Platform } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
 
 import { ListItem, Separator } from '../components/List';
 
+const ICON_PREFIX = Platform.OS === 'ios' ? 'ios' : 'md';
 const ICON_COLOR = '#868686';
 const ICON_SIZE = 23;
 
@@ -25,7 +26,7 @@ class Options extends Component {
           onPress={this.handleThemesPress}
           customIcon={
             <Ionicons
-              name="ios-arrow-forward"
+              name={`${ICON_PREFIX}-arrow-forward`}
               color={ICON_COLOR}
               size={ICON_SIZE}
             />
@@ -37,7 +38,7 @@ class Options extends Component {
           onPress={this.handleSitePress}
           customIcon={
             <Ionicons
-              name="ios-link"
+              name={`${ICON_PREFIX}-link`}
               color={ICON_COLOR}
               size={ICON_SIZE}
             />
