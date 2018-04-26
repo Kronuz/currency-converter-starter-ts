3476c9f6a1c8c7e8c239678e2f713242dd047d03
German M. Bravo <german.mb@gmail.com>
2018-04-26 12:23:06 -0500

    Handling Errors I: add Alert component

---
+++ app/components/Alert/connectAlert.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import hoistNonReactStatic from 'hoist-non-react-statics';
+
+const connectAlert = WrappedComponent => {
+  const ConnectedAlert = (props, context) => (
+    <WrappedComponent
+      {...props}
+      alertWithType={context.alertWithType}
+      alert={context.alert}
+    />
+  );
+  ConnectedAlert.contextTypes = {
+    alertWithType: PropTypes.func,
+    alert: PropTypes.func,
+  };
+  return hoistNonReactStatic(ConnectedAlert, WrappedComponent);
+};
+
+export default connectAlert;
