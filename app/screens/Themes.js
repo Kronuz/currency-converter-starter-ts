f49b4990cd37d1eb0ad2b1a3eaf61f1798503fa9
German M. Bravo <german.mb@gmail.com>
2018-04-26 14:06:36 -0500

    Themes Screen: Actions

--- app/screens/Themes.js
+++ app/screens/Themes.js
@@ -2,8 +2,10 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { ScrollView, StatusBar } from 'react-native';
 import EStyleSheet from 'react-native-extended-stylesheet';
+import { connect } from 'react-redux';
 
 import { ListItem, Separator } from '../components/List';
+import { changePrimaryColor } from '../actions/theme'
 
 const styles = EStyleSheet.create({
   $blue: '$primaryBlue',
@@ -14,6 +16,7 @@ const styles = EStyleSheet.create({
 
 class Themes extends Component {
   handlePressTheme = (color) => {
+    this.props.dispatch(changePrimaryColor(color));
     this.props.navigation.goBack(null);
   };
 
@@ -62,4 +65,4 @@ Themes.propTypes = {
   navigation: PropTypes.object,
 };
 
-export default Themes;
+export default connect()(Themes);
