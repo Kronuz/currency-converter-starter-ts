2e937723f9672579cd766f64c6fd24fa73cc1855
German M. Bravo <german.mb@gmail.com>
2018-04-26 11:57:58 -0500

    Currency List Modal: Navigation III: currency go back

--- app/screens/CurrencyList.js
+++ app/screens/CurrencyList.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import { FlatList, View, StatusBar } from 'react-native';
 
 import { ListItem, Separator } from '../components/List';
@@ -8,7 +9,7 @@ const TEMP_CURRENT_CURRENCY = 'CAD';
 
 class CurrencyList extends Component {
   handlePress = () => {
-    console.log('row press');
+    this.props.navigation.goBack(null);
   };
 
   render() {
@@ -31,5 +32,9 @@ class CurrencyList extends Component {
     );
   }
 }
+
+CurrencyList.propTypes = {
+  navigation: PropTypes.object,
+};
 
 export default CurrencyList;
