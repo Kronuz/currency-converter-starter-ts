e869987c0d4e791095cef3001cec0d1fd50519c2
German M. Bravo <german.mb@gmail.com>
2018-05-07 09:42:35 -0500

    Renamed ActionTyped -> TypedAction

--- app/actions/index.tsx
+++ app/actions/index.tsx
@@ -2,16 +2,16 @@ import { ReducersMapObject } from 'redux';
 
 // From https://medium.com/@martin_hotell/improved-redux-type-safety-with-typescript-2-8-2c11a8062575
 
-export interface ActionTyped<T extends string> {
+export interface TypedAction<T extends string> {
   type: T;
 }
 
-export interface AnyActionTyped<T extends string, P> extends ActionTyped<T> {
+export interface AnyTypedAction<T extends string, P> extends TypedAction<T> {
   payload: P,
 }
 
-export function createAction<T extends string>(type: T): ActionTyped<T>;
-export function createAction<T extends string, P>(type: T, payload: P): AnyActionTyped<T, P>;
+export function createAction<T extends string>(type: T): TypedAction<T>;
+export function createAction<T extends string, P>(type: T, payload: P): AnyTypedAction<T, P>;
 export function createAction<T extends string, P>(type: T, payload?: P) {
   return payload === undefined ? { type } : { type, payload };
 }
